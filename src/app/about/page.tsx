0a3997b603db989d977fd3b8cdfaa8bc35c86a88
killian vasse <killian.vasse@epitech.digital>
2023-04-19 11:10:03 +0200

    add text about page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -5,7 +5,15 @@ export default function page() {
 		<div className="mx-auto mb-8 max-w-6xl space-y-5 p-4">
 			<h1 className="text-2xl font-medium text-amber-600">About</h1>
 			<p>
-				Killian vasse - Mathieu Bobillier
+				Bienvenue sur notre site de référencement de films et de séries ! Nous sommes une petite équipe de deux développeurs passionnés qui ont créé cet outil pour aider les amateurs de cinéma et de télévision à trouver facilement les titres qu'ils recherchent.<br/><br/>
+
+				Notre site est conçu pour être simple et facile à utiliser. Vous pouvez naviguer à travers notre catalogue de films et de séries en utilisant une variété de filtres, tels que le genre, la durée, l'année de sortie et la notation des utilisateurs. Vous pouvez également utiliser notre barre de recherche pour trouver rapidement des titres spécifiques.<br/><br/>
+
+				Nous sommes fiers de dire que notre site est régulièrement mis à jour avec les dernières sorties cinématographiques et télévisuelles, ce qui signifie que vous pouvez toujours trouver les derniers films et séries à la mode.<br/><br/>
+
+				Nous sommes une équipe dévouée à offrir une expérience utilisateur de qualité supérieure, et nous sommes toujours ouverts aux commentaires et aux suggestions de nos utilisateurs. N'hésitez pas à nous contacter si vous avez des questions ou des commentaires.<br/><br/><br/>
+
+				Merci de visiter notre site et nous espérons que vous trouverez ce que vous cherchez !
 			</p>
 		</div>
 	)
